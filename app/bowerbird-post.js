000210f241d4408ca813f665786dc4b045f8afd6
Mikko Heikkinen <hawk@biomi.org>
2017-01-20 13:15:56 +0200

    Save data to mLab MongoDB

--- app/bowerbird-post.js
+++ app/bowerbird-post.js
@@ -7,14 +7,23 @@ function post(argumentsObj) {
     parameters = argumentsObj.parameters;
     payload = argumentsObj.payload;
 
-    console.log("Payload: " + JSON.stringify(payload));
+    console.log("Payload: " + payload);
+    let data = JSON.parse(payload);
 
     let status;
+
+    // Todo: Validate payload?, max size
+    // Todo Handle errors
 
     MongoClient.connect(process.env.MLAB_CONNECTION, function(err, db) {
         if (err == null)
         {
             status = "Connected correctly to server";
+            let options = {};
+            let collection = db.collection(process.env.MLAB_COLL);
+            collection.insertOne(data, options, function(err, result) {
+                if(err) throw err;
+            });
         }
         else
         {
