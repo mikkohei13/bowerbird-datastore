b0fd3378ac0789514f72eef5450970f00e416d21
Mikko Heikkinen <hawk@biomi.org>
2017-01-21 21:59:46 +0200

    Get query, sort 6 limit from GET params

--- app/bowerbird-get.js
+++ app/bowerbird-get.js
@@ -12,11 +12,32 @@ function get(argumentsObj) {
             parameters.logStatus(err, 503, "Database connection failed.");
         }
         else {
-            let query = {'bowerbird_set': 'owntracks'};
+            // Todo: expand, functionalize
+            let query = {"bowerbird_set": parameters.queryParts.bowerbird_set};
+
+            let limit = parseInt(parameters.queryParts.limit, 10);
+            if (! Number.isInteger(limit)) {
+                console.log("limit: " + limit);
+                limit = 1; // default
+            }
+
+            let sortString = parameters.queryParts.sort;
+            let sortParts = sortString.split(":");
+            let sortOrder;
+
+            if ("DESC" == sortParts[1]) {
+                sortOrder = -1;
+            }
+            else {
+                sortOrder = 1; // default
+            }
+            let sort = { [sortParts[0]] : sortOrder };
+
+//            find( { qty: { $gt: 25 } } )
 
             let collection = db.collection(process.env.MLAB_COLL);
 
-            collection.find(query).toArray(function(err, documents) {
+            collection.find(query).limit(limit).sort(sort).toArray(function(err, documents) {
                 if (err) {
                     parameters.logStatus(err, 500, "Database query failed.");
                 }
